c9898fb3e47930a20fed1efcb81bea2b48b2b6bc
Daniel Arnsberger <daniel.arnsberger@googlemail.com>
2015-08-31 18:29:15 +0200

    Add a dialog at startup to specify, to how many robots the user wants to connect

--- src/main/java/com/github/rosjava/robotino/run_robotino/LaunchRobotino.java
+++ src/main/java/com/github/rosjava/robotino/run_robotino/LaunchRobotino.java
@@ -3,6 +3,8 @@ package com.github.rosjava.robotino.run_robotino;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import javax.swing.JOptionPane;
+
 public class LaunchRobotino implements Runnable {
 	
 	private Thread t;
@@ -17,8 +19,19 @@ public class LaunchRobotino implements Runnable {
 	public void run() {
 		StringBuffer output = new StringBuffer();
 		Process p;
+		String command = "";
+		Object[] options = {"One robot", "Two robots", "Multiple Robots"};
+		int option = JOptionPane.showOptionDialog(null, "How many robots do you want to connect?", "Launch", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]) ;
+		switch (option) {
+		case 0: command = "roslaunch robotino_node robotino_node.launch";
+			break;
+		case 1: command = "roslaunch robotino_node robotino_two_nodes.launch";
+			break;
+		case 2: command = "roslaunch robotino_node robotino_lanes.launch";
+			break;
+		}
 		try {
-			p = Runtime.getRuntime().exec("roslaunch robotino_node robotino_node.launch");
+			p = Runtime.getRuntime().exec(command);
 			p.waitFor();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String line = "";
