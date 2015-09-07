10aebd3a68517da1f18acad389ed7db9a70f411f
Daniel Arnsberger <daniel.arnsberger@googlemail.com>
2015-09-07 16:03:44 +0200

    Move all dummy robots in one node

--- src/DriveWidget.java
+++ src/DriveWidget.java
@@ -53,25 +53,11 @@ public class DriveWidget extends JComponent implements NodeMain {
 	@Override
 	public void onStart(ConnectedNode connectedNode) {
 		final Publisher<geometry_msgs.Twist> publisher_robot_1 = connectedNode.newPublisher("cmd_vel_1", geometry_msgs.Twist._TYPE);
-		final Publisher<geometry_msgs.Twist> publisher_robot_2 = connectedNode.newPublisher("cmd_vel_2", geometry_msgs.Twist._TYPE);
-		final Publisher<geometry_msgs.Twist> publisher_robot_3 = connectedNode.newPublisher("cmd_vel_3", geometry_msgs.Twist._TYPE);
-		final Publisher<geometry_msgs.Twist> publisher_robot_4 = connectedNode.newPublisher("cmd_vel_4", geometry_msgs.Twist._TYPE);
-		final Publisher<geometry_msgs.Twist> publisher_robot_5 = connectedNode.newPublisher("cmd_vel_5", geometry_msgs.Twist._TYPE);
 		final Log log = connectedNode.getLog();
 		log.info("Hallo!");
 		createGUI(publisher_robot_1);
 		
-		connectedNode.executeCancellableLoop(new CancellableLoop() {
-			
-			@Override
-			protected void loop() throws InterruptedException {
-				Twist cmd = publisher_robot_2.newMessage();
-				cmd.getLinear().setX(1);
-				cmd.getLinear().setY(0);
-				cmd.getAngular().setZ(0);
-				publisher_robot_2.publish(cmd);
-			}
-		});
+		
 	}
 	
 
--- src/Main.java
+++ src/Main.java
@@ -12,23 +12,25 @@ public class Main {
 	//Main
 	private static DriveWidget widget = new DriveWidget();
 	private static Checker checker = new Checker();
+	private static DummyRobots dummies = new DummyRobots();
 	
 	public static void main(String[] args) {
-		LaunchRobotino launch = new LaunchRobotino("Launch robot");
-		Thread thread = new Thread(launch);
-		thread.setDaemon(true);
-		thread.start();
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		LaunchRobotino launch = new LaunchRobotino("Launch robot");
+//		Thread thread = new Thread(launch);
+//		thread.setDaemon(true);
+//		thread.start();
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 		NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
 		
 		NodeConfiguration widgetConfig = NodeConfiguration.newPublic("10.0.2.15");
-		Preconditions.checkState(widget != null && checker != null);
+		Preconditions.checkState(widget != null && checker != null && dummies != null);
+		nodeMainExecutor.execute(dummies, widgetConfig);
 		nodeMainExecutor.execute(widget, widgetConfig);
 		nodeMainExecutor.execute(checker, widgetConfig);
 	}
